d4f0cec214d9011aaae23e00f07551bd5c9ec0b5
Bradley Mulick <b.radleymulick@icloud.com>
2020-12-18 01:23:05 -0700

    better2

--- src/components/rates/Rates.js
+++ src/components/rates/Rates.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import "./rates.css"
+
 import styled from 'styled-components'
 
 const Rats = styled.div`
@@ -12,7 +12,27 @@ const Rats = styled.div`
 }
 .rates-boxs {
 padding: 44px;
-display: 30%
+display: 30%;
+font-size: 36px;
+}
+
+.rates-column h1 {
+    font-size: 54px;
+}
+
+.rates-container {
+  width: 100%;
+  height: 100vh;
+  background-color: thistle;
+}
+
+.header-img {
+  height: 60vh;
+  width: 100%;
+  background-repeat: no-repeat; /* Do not repeat the image */
+  background-size: 100%;
+  background-position: center;
+  background-image: url("https://images.squarespace-cdn.com/content/v1/555f7716e4b0c32dcc3cfe69/1524946374169-BIT9G88MDTHF5OHDPVP9/ke17ZwdGBToddI8pDm48kDHPSfPanjkWqhH6pl6g5ph7gQa3H78H3Y0txjaiv_0fDoOvxcdMmMKkDsyUqMSsMWxHk725yiiHCCLfrh8O1z4YTzHvnKhyp6Da-NYroOW3ZGjoBKy3azqku80C789l0mwONMR1ELp49Lyc52iWr5dNb1QJw9casjKdtTg1_-y4jz4ptJBmI9gQmbjSQnNGng/IMG_3491.jpg?format=2500w");
 }
 
 @media (max-width: 768px) {
@@ -34,24 +54,26 @@ export default function Rates() {
     return (
         <Rats>
             <div className="header-img"></div>
+            <h2>Packages Include</h2>
+            <p>Seven Night / Eight Day Vacation Packages includes International & Domestic Airfare, All Meals, Accommodations,  Full day guided tour to Corcovado National Park and a Full Day guided tour to Caño Island.  </p>
             <div className="rates-column">
                 <div className="rates-boxs">
                     <h1>Sunset</h1>
-                    <p>$4,424 per person</p>
+                    <p>$4,574 per person</p>
                     <p>
-                        $3,500 per child</p>
+                        $3,650 per child</p>
                 </div>
                 <div className="rates-boxs"><h1>
                     Deluxe
                     </h1>
                     <p>
-                        $4,109 per person
+                        $4,259 per person
                         </p>
                 </div>
                 <div className="rates-boxs"><h1>
                     Superior</h1>
                     <p>
-                        $3,739 per person</p>
+                        $3,889 per person</p>
                 </div>
             </div>
             <div className="rates-column">
@@ -62,18 +84,18 @@ export default function Rates() {
                 </h1>
                     <h2>38 person</h2>
                     <p>
-                        $4,058 per person</p>
+                        $4,208 per person</p>
                     <p>
-                        Total $154,152</p></div>
+                        Total $159,852</p></div>
                 <div
                     className="rates-boxs">
                     <h1>
                         Beach House
                 </h1>
                     <p>
-                        Master: $4,109 per person</p>
+                        Master: $4,259 per person</p>
                     <p>
-                        Apartment $3,739 per person</p>
+                        Apartment $3,889 per person</p>
                 </div>
                 <div
                     className="rates-boxs">
@@ -82,9 +104,9 @@ export default function Rates() {
                 </h1>
                     <h2>20 person</h2>
                     <p>
-                        $4,061 per person</p>
+                        $4,211 per person</p>
                     <p>
-                        Total $81,220</p></div>
+                        Total $84,220</p></div>
             </div>
 
 
